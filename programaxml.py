e0d824d60dced00dcd2215c295fe2d7a181e4b35
Álvaro Rodríguez <dem13k27@gmail.com>
2016-02-22 12:17:39 +0100

    he añadido un menú intereactivo

--- programaxml.py
+++ programaxml.py
@@ -12,3 +12,42 @@
 4.Pide el número de electos y te muestra si hay, los partidos que tienen ese número.
 
 5.Muestra la información de forma gráfica ,además de mostrar datos generales sobre las elecciones, votos totales, votos en blanco , etc.."""
+
+#importación de librerías
+#import xpath
+import os
+from lxml import etree
+
+#Carga el fichero xml en una variable
+arbol = etree.parse('ficheroxml.xml')
+
+
+#Bucle del programa
+salir = False
+
+while salir == False:
+    os.system('clear')
+    print "+-------------------------------------------+"
+    print "| Lector de resultados electorales.         |"
+    print "|                                           |"
+    print "|  Opciones disponibles:                    |"
+    print "|  -Conteo de partidos - 1                  |"
+    print "|  -Conteo de partidos - 2                  |"
+    print "|  -Conteo de partidos - 3                  |"
+    print "|  -Conteo de partidos - 4                  |"
+    print "|  -Conteo de partidos - 5                  |"
+    print "|                                           |"
+    print "|(Para salir usal la letra 'q')             |"
+    print "+-------------------------------------------+"
+    respuesta = raw_input("respuesta: ")
+
+    #Comprobador de respuesta
+    if respuesta == "q":
+        salir = True
+
+    elif respuesta == 1:
+        os.system('clear')
+        print "+-----------------------------+"
+        print "| Conteo de partidos.         |"
+        print "+-----------------------------+"
+        raw_input("Pulse enter para continuar")
