95dd9b32b0b5a248b4d1bb56b3642b39260fa765
Álvaro Rodríguez <dem13k27@gmail.com>
2016-02-22 10:58:55 +0100

    PRograma con enunciado añadido,fichero xml añadido

---
+++ programaxml.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#"Pre-Enunciado"
+
+"""1.Lista el número de partidos que aparecen en el escrutinio.
+
+2.Muestra el nombre da cada partido y el número de electos de cada uno.
+
+3.Pide el nombre de un partido y muestra sus datos:nombre, número de electos, porcentaje de votos.
+
+4.Pide el número de electos y te muestra si hay, los partidos que tienen ese número.
+
+5.Muestra la información de forma gráfica ,además de mostrar datos generales sobre las elecciones, votos totales, votos en blanco , etc.."""
